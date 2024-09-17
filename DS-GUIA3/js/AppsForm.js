0b1f91e962a9f9511678f2e8275a005650be5310
Abrilskop <uberpot.ia@gmail.com>
2024-09-16 19:49:00 -0500

    Llevar funcion de AppsForm2 a AppForm

--- DS-GUIA3/js/AppsForm.js
+++ DS-GUIA3/js/AppsForm.js
@@ -15,3 +15,27 @@ function validarFormulario(e){
     const url1 = `paginas/Pagina1.html?nombre1=${encodeURIComponent(nombre1)}&apellido1=${encodeURIComponent(apellido1)}&nacionalidad1=${encodeURIComponent(nacionalidad1)}`;
     window.open(url1, '_blank');
 }
+
+// formulario 2
+const formulario2 = document.querySelector("#formulario2");
+
+if (formulario2) {
+    formulario2.addEventListener("submit", validarFormulario2);
+}
+
+function validarFormulario2(e) {
+    e.preventDefault();
+
+    const nombre2 = document.querySelector("#vnombre2").value;
+    const apellido2 = document.querySelector("#vapellido2").value;
+    const nacionalidad2 = document.querySelector("#vnacionalidad2").value;
+
+    const respuesta2 = document.getElementById("respuesta2");
+
+    if (respuesta2) {
+        respuesta2.textContent = `Hola ${nombre2}, tu apellido es ${apellido2} y tu nacionalidad es ${nacionalidad2}`;
+    }
+
+    const url2 = `paginas/Pagina2.html?nombre2=${encodeURIComponent(nombre2)}&apellido2=${encodeURIComponent(apellido2)}&nacionalidad2=${encodeURIComponent(nacionalidad2)}`;
+    window.open(url2, '_blank');
+}
