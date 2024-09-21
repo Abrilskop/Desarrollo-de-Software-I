047fed96e8bd5978dcc09c1058317f13b089a585
Abrilskop <uberpot.ia@gmail.com>
2024-09-20 23:10:19 -0500

    Refactor: Llevar funcion de AppsForm2,3,4,5,10,11,12 a AppForm.js

--- DS-GUIA3/js/AppsForm.js
+++ DS-GUIA3/js/AppsForm.js
@@ -39,3 +39,42 @@ function validarFormulario2(e) {
     const url2 = `paginas/Pagina2.html?nombre2=${encodeURIComponent(nombre2)}&apellido2=${encodeURIComponent(apellido2)}&nacionalidad2=${encodeURIComponent(nacionalidad2)}`;
     window.open(url2, '_blank');
 }
+
+// formualario 3
+const formulario3 = document.querySelector("#formulario3");
+formulario3.addEventListener("submit", validarFormulario3);
+function validarFormulario3(e) {
+    e.preventDefault();
+
+    const nombre3 = document.querySelector("#vnombre3").value;
+    const apellido3 = document.querySelector("#vapellido3").value;
+    const nacionalidad3 = document.querySelector("#vnacionalidad3").value;
+
+    const respuesta3 = document.getElementById("respuesta3");
+    respuesta3.textContent = `Hola ${nombre3}, tu apellido es ${apellido3} y tu nacionalidad es ${nacionalidad3}`;
+
+    const url3 = `paginas/Pagina3.html?nombre3=${encodeURIComponent(nombre3)}&apellido3=${encodeURIComponent(apellido3)}&nacionalidad3=${encodeURIComponent(nacionalidad3)}`;
+
+    window.open(url3, '_blank');
+}
+
+// formulario 4
+const formulario4 = document.querySelector("#formulario4");
+
+formulario4.addEventListener("submit", validarFormulario4);
+function validarFormulario4(e) {
+    e.preventDefault();
+
+    const nombre4 = document.querySelector("#vnombre4").value;
+    const apellido4 = document.querySelector("#vapellido4").value;
+    const contraseña4 = document.querySelector("#vcontraseña4").value;
+
+    const respuesta4 = document.getElementById("respuesta4");
+    respuesta4.textContent = `Hola ${nombre4}, tu apellido es ${apellido4} y tu contraseña es ${contraseña4}`;
+
+    const url4 = `paginas/Pagina4.html?nombre4=${encodeURIComponent(nombre4)}&apellido4=${encodeURIComponent(apellido4)}&contraseña4=${encodeURIComponent(contraseña4)}`;
+
+    window.open(url4, '_blank');
+}
+
+// formulario 5
--- DS-GUIA3/js/AppsForm10.js
+++ DS-GUIA3/js/AppsForm10.js
@@ -1,24 +1,58 @@
+// formulario 10
 const formulario10 = document.querySelector("#formulario10");
 
-// Crear el evento
 formulario10.addEventListener("submit", validarFormulario10);
 
-// Función para validar y procesar el formulario
 function validarFormulario10(e) {
     e.preventDefault();
 
-    // Obtener los valores del formulario
     const nombre10 = document.querySelector("#vnombre10").value;
     const apellido10 = document.querySelector("#vapellido10").value;
     const nacionalidad10 = document.querySelector("#vnacionalidad10").value;
 
-    // Mostrar el resultado en el mismo documento
     const respuesta10 = document.getElementById("respuesta10");
     respuesta10.textContent = `Hola ${nombre10}, tu apellido es ${apellido10} y tu nacionalidad es ${nacionalidad10}`;
 
-    // Construir la URL para abrir en una nueva pestaña
     const url10 = `paginas/Pagina10.html?nombre10=${encodeURIComponent(nombre10)}&apellido10=${encodeURIComponent(apellido10)}&nacionalidad10=${encodeURIComponent(nacionalidad10)}`;
 
-    // Abrir la nueva pestaña con la URL construida
     window.open(url10, '_blank');
+}
+// formualrio 11
+const formulario11 = document.querySelector("#formulario11");
+
+formulario11.addEventListener("submit", validarFormulario11);
+
+function validarFormulario11(e) {
+    e.preventDefault();
+
+    const nombre11 = document.querySelector("#vnombre11").value;
+    const apellido11 = document.querySelector("#vapellido11").value;
+    const nacionalidad11 = document.querySelector("#vnacionalidad11").value;
+
+    const respuesta11 = document.getElementById("respuesta11");
+    respuesta11.textContent = `Hola ${nombre11}, tu apellido es ${apellido11} y tu nacionalidad es ${nacionalidad11}`;
+
+    const url11 = `paginas/Pagina11.html?nombre11=${encodeURIComponent(nombre11)}&apellido11=${encodeURIComponent(apellido11)}&nacionalidad11=${encodeURIComponent(nacionalidad11)}`;
+
+    window.open(url11, '_blank');
+}
+
+// formulario 12
+const formulario12 = document.querySelector("#formulario12");
+
+formulario12.addEventListener("submit", validarFormulario12);
+
+function validarFormulario12(e) {
+    e.preventDefault();
+
+    const nombre12 = document.querySelector("#vnombre12").value;
+    const apellido12 = document.querySelector("#vapellido12").value;
+    const nacionalidad12 = document.querySelector("#vnacionalidad12").value;
+
+    const respuesta12 = document.getElementById("respuesta12");
+    respuesta12.textContent = `Hola ${nombre12}, tu apellido es ${apellido12} y tu nacionalidad es ${nacionalidad12}`;
+
+    const url12 = `paginas/Pagina12.html?nombre12=${encodeURIComponent(nombre12)}&apellido12=${encodeURIComponent(apellido12)}&nacionalidad12=${encodeURIComponent(nacionalidad12)}`;
+
+    window.open(url12, '_blank');
 }
